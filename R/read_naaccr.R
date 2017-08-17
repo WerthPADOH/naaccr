ad9af6b04599876c615cc627cdcfa22119d841a0
Nathan Werth <nwerth@pa.gov>
2017-08-17 10:57:32 -0400

    Allowed reading more types of input

--- R/read_naaccr.R
+++ R/read_naaccr.R
@@ -1,12 +1,44 @@
+#' Create a connection specified in one of several ways
+#' @param input Either a string with a file name (containing no \code{\n}
+#'   character), a \code{\link[base]{connection}} object, or the text records
+#'   themselves as a character vector.
+#' @return A \code{connection} object
+#' @seealso
+#'   \code{\link[base]{connections}},
+#'   \code{\link[base]{textConnections}}
+as.connection <- function(input) {
+  # Based on logic in utils::read.table
+  if (is.character(input)) {
+    input <- if (length(input) > 1L || grepl('\n', input[1L], fixed = TRUE)) {
+      textConnection(input)
+    } else {
+      file(input, 'rt')
+    }
+  }
+  if (!inherits(input, 'connection')) {
+    stop("'input' must be a filepath, a connection, or a character vector")
+  }
+  if (!isOpen(input, 'rt')) {
+    open(input, 'rt')
+  }
+  input
+}
+
+
 #' Read records from a NAACCR file
 #' @import readr
 #' @import data.table
-read_naaccr <- function(file, naaccr_version = NULL) {
+read_naaccr <- function(input, naaccr_version = NULL) {
+  input <- as.connection(input)
+  on.exit(
+    if (isOpen(input)) close(input),
+    add = TRUE
+  )
   if (is.null(naaccr_version)) {
     naaccr_version <- max(naaccr_items[['naaccr_version']])
   }
   # NAACCR record types differ in line length
-  line1 <- readLines(file, 1L)
+  line1 <- readLines(input, 1L)
   line_length <- nchar(line1)
   lookup_key <- list(naaccr_version)
   input_items <- naaccr_items[
@@ -26,18 +58,9 @@ read_naaccr <- function(file, naaccr_version = NULL) {
       col_names = gsub('\\W+', '_', name)
     )
   ]
-
-  if (inherits(file, 'connection')) {
-    if (isOpen(file)) {
-      pushBack(file, line1)
-    }
-    on.exit(
-      if (isOpen(file)) close(file),
-      add = TRUE
-    )
-  }
+  pushBack(input, line1)
   readr::read_fwf(
-    file          = file,
+    file          = input,
     col_positions = col_positions,
     col_types     = readr::cols(.default = readr::col_character()),
     na            = ''
