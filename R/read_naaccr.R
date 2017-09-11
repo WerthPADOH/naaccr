71599b33308b1ddad83d5c3d049056b1251121c9
Nathan Werth <nwerth@pa.gov>
2017-09-11 13:56:27 -0400

    Using stringi to read fixed-width instead of readr
    It's fast, not much more complicated, and works with any type of
    connection, open or not.

--- R/read_naaccr.R
+++ R/read_naaccr.R
@@ -25,6 +25,28 @@ as.connection <- function(input) {
 }
 
 
+parse_records <- function(records, start_cols, end_cols, col_names = NULL) {
+  if (length(start_cols) != length(end_cols)) {
+    stop("start_cols and end_cols must be the same length")
+  }
+  if (!is.null(col_names) && length(start_cols) != length(col_names)) {
+    stop("start_cols and end_cols must be the same length")
+  }
+  item_matrix <- mapply(
+    starts = start_cols,
+    ends   = end_cols,
+    FUN = function(starts, ends) {
+      values <- stringi::stri_sub(records, starts, ends)
+      stringi::stri_subset_regex(values, "^\\s+") <- NA
+      values
+    },
+    SIMPLIFY = FALSE
+  )
+  names(item_matrix) <- col_names
+  as.data.frame(item_matrix, stringsAsFactors = FALSE)
+}
+
+
 #' Read records from a NAACCR file
 #' @param input Either a string with a file name (containing no \code{\\n}
 #'   character), a \code{\link[base]{connection}} object, or the text records
@@ -32,7 +54,6 @@ as.connection <- function(input) {
 #' @param naaccr_version An integer specifying which NAACCR format should be
 #'   used to parse the records.
 #' @return A \code{data.frame} of the records.
-#' @import readr
 #' @import data.table
 #' @import stringi
 read_naaccr <- function(input, naaccr_version = NULL) {
@@ -52,6 +73,8 @@ read_naaccr <- function(input, naaccr_version = NULL) {
     on = 'naaccr_version'
   ][
     order(start_col)
+  ][
+    start_col > cummax(data.table::shift(end_col, fill = 0L))
   ]
   # Read all record types as the longest type, with padding
   record_lines <- readLines(input)
@@ -60,19 +83,10 @@ read_naaccr <- function(input, naaccr_version = NULL) {
     record_lines,
     width = max(line_lengths) - line_lengths
   )
-  record_lines <- stringi::stri_join(record_lines, collapse = "\n")
-  col_positions <- input_items[
-    start_col > cummax(data.table::shift(end_col, fill = 0L)),
-    readr::fwf_positions(
-      start     = start_col,
-      end       = end_col,
-      col_names = r_name
-    )
-  ]
-  readr::read_fwf(
-    file          = record_lines,
-    col_positions = col_positions,
-    col_types     = readr::cols(.default = readr::col_character()),
-    na            = ''
+  parse_records(
+    records    = record_lines,
+    start_cols = input_items[["start_col"]],
+    end_cols   = input_items[["end_col"]],
+    col_names  = input_items[["r_name"]]
   )
 }
