6ace9132362cf6d5c8f1e5dffb489acae65b1c29
Nathan Werth <nwerth@pa.gov>
2018-06-28 15:20:50 -0400

    Made function for sourcing files in a subprocess

--- build.R
+++ build.R
@@ -3,11 +3,17 @@ library(devtools)
 library(testthat)
 
 
+source_subprocess <- function(source_file, ...) {
+  system2(command = "Rscript", args = source_file, ...)
+}
+
+
 # Internal data ----------------------------------------------------------------
 # Run "data-raw/request-naaccr-info.R" to update the file
 # "data-raw/naaccr_info_from_api.csv". Because of our proxy, the script is not
 # reliable enough to include in an automated build.
-system2(command = "Rscript", args = "data-raw/naaccr_items.R")
+source_subprocess("data-raw/naaccr_items.R")
+source_subprocess("data-raw/create-country_codes.R")
 naaccr_items <- readRDS("data-raw/naaccr_items.rds")
 item_types   <- readRDS("data-raw/item_types.rds")
 devtools::use_data(naaccr_items, item_types, internal = TRUE, overwrite = TRUE)
