86c5f6ac029444358a2d69987390d048cd069207
Nathan Werth <nwerth@pa.gov>
2018-06-28 14:13:10 -0400

    Querying SEER's API no longer done in build script
    The department's proxy is too fickle to have this be part of the automated
    build process. Instead, the script should be run manually when necessary
    (once after each new NAACCR version comes out) with user/password info.

--- build.R
+++ build.R
@@ -2,14 +2,13 @@
 library(devtools)
 
 
-source_subprocess <- function(source_file, ...) {
-  system2(command = "Rscript", args = source_file, ...)
-}
-
-
-# Internal data
-source_subprocess("data-raw/request-naaccr-info.R")
-source_subprocess("data-raw/naaccr_items.R")
+# Internal data ----------------------------------------------------------------
+# Run "data-raw/request-naaccr-info.R" to update the file
+# "data-raw/naaccr_info_from_api.csv". Because of our proxy, the script is not
+# reliable enough to include in an automated build.
+system2(command = "Rscript", args = "data-raw/naaccr_items.R")
 naaccr_items <- readRDS("data-raw/naaccr_items.rds")
 item_types   <- readRDS("data-raw/item_types.rds")
 devtools::use_data(naaccr_items, item_types, internal = TRUE, overwrite = TRUE)
+
+# Tests ------------------------------------------------------------------------
