3ea446217bed3db85efe3028075e9bba46e73758
Nathan Werth <nwerth@pa.gov>
2018-07-02 16:55:18 -0400

    Added test for auto-cleaning

--- tests/testthat/test-naaccr_record.R
+++ tests/testthat/test-naaccr_record.R
@@ -20,3 +20,21 @@ test_that("name_recent gets the right names from item numbers", {
     "rxDateRadiationEndedFlag"
   )
 })
+
+test_that("as.naaccr_record auto-cleans fields", {
+  record <- data.frame(
+    comorbidComplication3 = c("00000", "12345"),
+    addrAtDxCity          = c("UNKNOWN", "Nowhere"),
+    followUpContactPostal = c("888888888", "17110"),
+    censusTract2000       = c("000000", "123456"),
+    followingRegistry     = c("0099999999", "1111111111"),
+    causeOfDeath          = c("7797", "C010"),
+    cancerStatus          = c("9", "2"),
+    stringsAsFactors = FALSE
+  )
+  processed <- as.naaccr_record(record)
+  for (column in names(record)) {
+    expect_true( is.na(processed[[column]][[1L]]))
+    expect_false(is.na(processed[[column]][[2L]]))
+  }
+})
