303da816bdfa8dc62bcc28af8be8661548570a2e
Nathan Werth <nwerth@pa.gov>
2018-12-13 15:02:04 -0500

    Add `write_naaccr` function
    The function writes a `naaccr_record` object to a connection in fixed-width
    format. Also includes documentation and tests.

---
+++ tests/testthat/test-write_naaccr.R
@@ -0,0 +1,61 @@
+library(testthat)
+library(naaccr)
+
+
+context("write_naaccr")
+
+
+write_naaccr_to_vector <- function(records, version = 18) {
+  tc <- textConnection(NULL, open = "w")
+  on.exit(close(tc))
+  write_naaccr(records, tc, version)
+  textConnectionValue(tc)
+}
+
+
+test_that("write_naaccr gives a warning for overly wide values", {
+  records <- data.frame(
+    patientIdNumber = c("1234567890", "2", "1234567890", "12345678", ""),
+    stringsAsFactors = FALSE
+  )
+  expect_warning(
+    write_naaccr_to_vector(records),
+    "\\b2\\b.*patientIdNumber.*too wide"
+  )
+})
+
+test_that("write_naaccr weaves sentineled values and flags", {
+  literals <- c("  ", "10", "95", "97", "98", "88", "90", "99")
+  records <- split_sentineled(literals, "regionalNodesPositive")
+  output <- write_naaccr_to_vector(records)
+  regionalNodesPositive <- substr(output, 998, 999)
+  expect_identical(regionalNodesPositive, literals)
+})
+
+test_that("write_naaccr recodes factors", {
+  literals <- c("1", "3", "5", " ", "6", "4", "2")
+  records <- data.frame(
+    maritalStatusAtDx = naaccr_factor(literals, "maritalStatusAtDx")
+  )
+  output <- write_naaccr_to_vector(records)
+  maritalStatusAtDx <- substr(output, 206, 206)
+  expect_identical(maritalStatusAtDx, literals)
+})
+
+test_that("write_naaccr correctly formats dates and times", {
+  records <- data.frame(
+    dateOfDiagnosis = as.Date(
+      c("2011-08-05", "2017-12-31", NA),
+      format = "%Y-%m-%d"
+    ),
+    pathDateSpecCollect1 = as.POSIXct(
+      c("2013-12-11 10:09:08", "2001-09-07 16:58:53", NA),
+      format = "%Y-%m-%d %H:%M:%S"
+    )
+  )
+  output <- write_naaccr_to_vector(records)
+  dates <- substr(output,  544,  551)
+  times <- substr(output, 6200, 6213)
+  expect_identical(dates, c("20110805", "20171231", "        "))
+  expect_identical(times, c("20131211100908", "20010907165853", "              "))
+})
